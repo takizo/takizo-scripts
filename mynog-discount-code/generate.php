4821ee44ca905668a145a0dca9261134972a9bae
Paul Ooi <paul@takizo.com>
2016-04-25 09:44:39 +0800

    promo code.

---
+++ mynog-discount-code/generate.php
@@ -0,0 +1,18 @@
+<?php
+function generateRandomString($length = 10) {
+    $characters = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    $charactersLength = strlen($characters);
+    $randomString = 'MYNOGCONF-';
+    for ($i = 0; $i < $length; $i++) {
+        $randomString .= $characters[rand(0, $charactersLength - 1)];
+    }
+    return $randomString;
+}
+
+
+
+for ($d=0; $d<100; $d++)
+{
+	echo generateRandomString(6)."\n";
+}
+?>
