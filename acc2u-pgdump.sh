75f77847eda2579c0d7c4ad9c14791635111cb7b
Paul Ooi <paul@takizo.com>
2016-04-25 09:45:42 +0800

    add acc2u pgdump.

---
+++ acc2u-pgdump.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+if [ -z "$1" ]; then
+        echo "Usage: db.sh <dbname> <userid>"
+fi
+
+if [ -z "$2" ]; then
+        echo "Usage: db.sh <dbname> <userid>"
+fi
+
+DBNAME=$1
+USERID=$2
+PGDUMP=/usr/bin/pg_dump
+
+${PGDUMP} -o -O -x -U ${USERID} ${DBNAME} > ${DBNAME}.sql
