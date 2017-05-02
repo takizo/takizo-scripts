e9e72e5bf3c25a13e633f5275a835f11ddbd5a62
CJ <paul@takizo.com>
2017-05-02 11:02:06 +0800

    add spam ass

---
+++ spamassassin/train-bayes-spam.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+## Submit SPAM email which forwarded to antispam@<domain.com>
+## Mailbox running Maildir format
+
+
+# Submit email to learn with bayes
+/usr/bin/sa-learn --spam --showdots --dbpath /var/spool/amavisd/.spamassassin/ /mail/domain.com/antispam/Maildir/{cur,new} -D
+
+
+# after submission for learning. Clean old email (yesterday email which already learned)
+/bin/find /mail/domain.com/antispam/Maildir/new/ -type f -mtime +1 -exec rm -rf {} \;
