dea7cf0f41319d8a7c98e70103e351e987686113
CJ <paul@takizo.com>
2017-07-21 13:28:09 +0800

    add table css

--- mynog-discount-code/generate.php
+++ mynog-discount-code/generate.php
@@ -2,7 +2,7 @@
 function generateRandomString($length = 10) {
     $characters = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ';
     $charactersLength = strlen($characters);
-    $randomString = 'MYNOGCONF-';
+    $randomString = 'IXNOGCONF-';
     for ($i = 0; $i < $length; $i++) {
         $randomString .= $characters[rand(0, $charactersLength - 1)];
     }
